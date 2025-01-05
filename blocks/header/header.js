59c56a9fcda6547fcb552bcafe52fee7f2898ebf
Doug Hatcher <superterran@gmail.com>
2025-01-05 04:59:59 +0000

    cleans up header

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -145,4 +145,55 @@ export default async function decorate(block) {
   navWrapper.className = 'nav-wrapper';
   navWrapper.append(nav);
   block.append(navWrapper);
+
+  // style the dynamic header
+
+  document.querySelector('header').classList.add('finisher-header');
+
+  const headerobserver = new MutationObserver((mutations, observer) => {
+    const finisherHeader = document.querySelector('.finisher-header');
+    if (finisherHeader) {
+      observer.disconnect();
+      // eslint-disable-next-line no-new, no-undef
+      new FinisherHeader({
+        count: 10,
+        size: {
+          min: 1300,
+          max: 1500,
+          pulse: 0,
+        },
+        speed: {
+          x: {
+            min: 0.1,
+            max: 0.6,
+          },
+          y: {
+            min: 0.1,
+            max: 0.6,
+          },
+        },
+        colors: {
+          background: '#9138e5',
+          particles: [
+            '#ff4848',
+            '#000000',
+            '#2235e5',
+            '#000000',
+            '#ff0000',
+          ],
+        },
+        blending: 'overlay',
+        opacity: {
+          center: 0.5,
+          edge: 0.05,
+        },
+        skew: 0,
+        shapes: [
+          'c',
+        ],
+      });
+    }
+  });
+
+  headerobserver.observe(document.body, { childList: true, subtree: true });
 }
